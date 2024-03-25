537c463ce363ea9513f231996ef25152f1ced085
adirama11 <jidaneadir11@gmail.com>
2024-03-25 22:38:42 +0700

    Update halaman produk dan menghubungkannya dengan API

--- src/components/ProductComponents/MainScreen/CardComponents/CardProduct.js
+++ src/components/ProductComponents/MainScreen/CardComponents/CardProduct.js
@@ -1,63 +1,74 @@
-import React from "react";
-import { ScrollView, View, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { ScrollView, View, Image, Text } from "react-native";
 import { Card, Title } from "react-native-paper";
 import GlobalStyle from "../../../../styles/GlobalStyle";
 import ButtonAdd from "../ButtonComponents/ButtonAdd";
 import ButtonEdit from "../ButtonComponents/ButtonEdit";
 import ButtonDelete from "../ButtonComponents/ButtonDelete";
+import { API_BASE_URL } from "../../../../api/apiConfig";
 
 export default function CardProduct() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/product`)
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
+
   return (
-      <ScrollView showsVerticalScrollIndicator={false}>
-
-        {/* Button Add */}
-        <ButtonAdd/>
-
-        {/* Card */}
-        {Array.from(Array(15)).map((_, i) => (
-          <Card key={i} style={GlobalStyle.cardProductContainer}>
-            <Card.Content style={{ flexDirection: "row" }}>
-              <View style={{ marginRight: 50 }}>
-                <Image
-                  style={{ width: 75, height: 100 }}
-                  source={require("../../../../assets/logo.jpg")}
-                />
-              </View>
-              <View style={{ flex: 1 }}>
-                <Title style={{
-                  fontSize:16,
-                  fontWeight:"bold",
-                  marginTop:0,
-                  marginBottom:0,
-                }}>Nama Makanan</Title>
-                <Title
+    <ScrollView showsVerticalScrollIndicator={false}>
+      {/* Button Add */}
+      <ButtonAdd />
+      {/* Card */}
+      {products.map((product, index) => (
+        <Card key={index} style={GlobalStyle.cardProductContainer}>
+          <Card.Content style={{ flexDirection: "row" }}>
+            <View style={{ marginRight: 50 }}>
+              {/* Ganti source gambar dengan gambar produk */}
+              <Image
+                 style={{ width: 75, height: 100 }}
+                 source={require("../../../../assets/logo.jpg")}
+              />
+            </View>
+            <View style={{ flex: 1 }}>
+              <Title
                 style={{
-                  fontSize:10,
-                  fontWeight:"bold",
-                  marginBottom:0,
-                }}>Stok : 20</Title>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    justifyContent: "flex-start",
-                    marginTop: 5,
-                    marginBottom: 5,
-                  }}
-                >
-                  {/* Button Edit */}
-                  <ButtonEdit
-                   onPress={() => navigation.navigate("Main", { screen: "CashierScreen" })}
-                  />
-
-                  {/* Button Delete */}
-                  <ButtonDelete
-                 
-                  />
-                </View>
+                  fontSize: 16,
+                  fontWeight: "bold",
+                  marginTop: 0,
+                  marginBottom: 0,
+                }}
+              >
+                {product.nama_produk}
+              </Title>
+              <Title
+                style={{
+                  fontSize: 10,
+                  fontWeight: "bold",
+                  marginBottom: 0,
+                }}
+              >
+                Harga: {product.harga}
+              </Title>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "flex-start",
+                  marginTop: 5,
+                  marginBottom: 5,
+                }}
+              >
+                {/* Button Edit */}
+                <ButtonEdit />
+                {/* Button Delete */}
+                <ButtonDelete />
               </View>
-            </Card.Content>
-          </Card>
-        ))}
-      </ScrollView>
+            </View>
+          </Card.Content>
+        </Card>
+      ))}
+    </ScrollView>
   );
 }
